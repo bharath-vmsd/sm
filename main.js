5f26021ec1596ed902333b24cf023d8bd776305f
Bharath <bharath@vmsd.in>
2025-10-30 23:44:34 +0530

    mudichur and manivakkam update

--- main.js
+++ main.js
@@ -11,4 +11,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const signageBuilder = new SignageBuilder(scene);
     signageBuilder.build();
+
+    // Keyboard shortcuts for navigation
+    document.addEventListener('keydown', (event) => {
+        let targetUrl = '';
+        switch (event.key) {
+            case '1':
+                targetUrl = './Assesment/adayar/';
+                break;
+            case '2':
+                targetUrl = './Assesment/adayar2/';
+                break;
+            case '3':
+                targetUrl = './Assesment/besant-nagar/';
+                break;
+            case '4':
+                targetUrl = './Assesment/mudichur/';
+                break;
+            case '5':
+                targetUrl = './Assesment/manivakkam/';
+                break;
+            case '6':
+                targetUrl = './Assesment/entrance/';
+                break;
+            case '7':
+                targetUrl = './Assesment/sunglass/';
+                break;
+            case '8':
+                targetUrl = './Assesment/logistics/';
+                break;
+        }
+
+        if (targetUrl) {
+            window.location.href = targetUrl;
+        }
+    });
 });
